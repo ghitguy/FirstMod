64186b03b671b2cb621168661fcc6cc2a60f6773
ghitguy <captika@outlook.com>
2020-05-09 18:41:45 +0100

    Added recipes, lang and loot table

---
+++ src/main/java/me/ghit/mymod/blocks/ModBlocks.java
@@ -0,0 +1,10 @@
+package me.ghit.mymod.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+
+    @ObjectHolder("mymod:firstblock")
+    public static FirstBlock firstBlock;
+
+}
---
+++ src/main/java/me/ghit/mymod/setup/IProxy.java
@@ -0,0 +1,9 @@
+package me.ghit.mymod.setup;
+
+import net.minecraft.world.World;
+
+public interface IProxy {
+
+    World getClientWorld();
+
+}
